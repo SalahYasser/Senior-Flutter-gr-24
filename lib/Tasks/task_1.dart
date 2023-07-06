e20b43026d430e323d462c30bef633197aad91c5
Salah Yasser <Salahy.allaithy@gmail.com>
2023-07-07 02:51:39 +0300

    Add Task 1

---
+++ lib/Tasks/task_1.dart
@@ -0,0 +1,14 @@
+void main() {
+
+  // Task 1.1 => convert degree from Fahrenheit to Celsius
+  var fahrenheit = 90;
+  var celsius = (fahrenheit - 32) * (5 / 9);
+
+  print('$celsius');
+
+  // Task 1.2 => calculate area of circle
+  var r = 5;
+  var area_circle = 3.14 * r * r;
+
+  print('$area_circle');
+}
