b984ccc99bf1b8786f5bea94f2b364f825f67113
Salah Yasser <Salahy.allaithy@gmail.com>
2023-07-08 01:17:22 +0300

    Add Task 2

---
+++ lib/Tasks/task2.dart
@@ -0,0 +1,46 @@
+void main() {
+
+  // Task 2.1  => program takes variable for degree out of 100 and print grade(A,B،C,D) using switch
+//   double degree = 70;
+//
+//   switch (degree) {
+//     case >= 85:
+//   {
+//   print("A");
+//   }
+//   break;
+//   case < 85 && >= 75:
+//   {
+//   print("B");
+//   }
+//   break;
+//   case < 75 && >= 65:
+//   {
+//   print('C');
+//   }
+//   break;
+//   case < 65 && >= 50:
+//   {
+//   print("D");
+//   }
+//   break;
+//   default:
+//   {
+//   print('F');
+//   }
+// }
+
+  // Task 2.2  => program takes 3 variables and print the average
+
+  var list = [9, 10, 11];
+  print((list[0] + list[1] + list[2]) / 3);
+
+  // Task 2.3  => program takes 2 variables and compare between them
+
+  var x = 6;
+  var y = 8;
+  if (x > y) {
+    print('$x is greater than $y');
+  } else
+    print('$y is greater than $x');
+}
